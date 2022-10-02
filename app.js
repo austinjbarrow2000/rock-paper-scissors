3135d10028df6310ff54baf01e1350eb00740eb6
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 14:06:15 -0400

    Add button and gameplay ui and rework backend

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector("#results");
+console.log(results);
+const gameplay = document.querySelector("#gameplay");
+
+buttons.forEach((button) => button.addEventListener('click', function(e) {
+    const div = document.createElement("div");
+    div.innerText = playRound(e.target.textContent);
+    gameplay.appendChild(div);
+    checkWin(); 
+}));
+
+game();
+
+function checkWin() {
+    if(computerScore == 5) {
+        const div = document.createElement("div");
+        div.innerText = playRound("The computer wins the game!");
+        gameplay.appendChild(div);
+        game();
+    }
+    
+    if(playerScore == 5) {
+        const div = document.createElement("div");
+        div.innerText = playRound("You win the game!");
+        gameplay.appendChild(div);
+        game();
+    }
+
+}
+
 function getComputerChoice() {
     let computerResponses = ["rock", "paper", "scissors"];
     max = 2;
@@ -17,22 +48,26 @@ function playRound(playerSelection) {
                 (playerSelection == "scissors" && computerChoice == "paper") ||
                 (playerSelection == "paper" && computerChoice == "rock")) {
         playerScore += 1;
+        results.innerText = `The score is Player: ${playerScore} Computer: ${computerScore}`;
         return `You win! ${playerSelection} beats ${computerChoice}`
     } 
     computerScore += 1;
+    results.innerText = `The score is Player: ${playerScore} Computer: ${computerScore}`;
     return `You lose! ${computerChoice} beats ${playerSelection}`
 } 
 
 function game() {
-    console.log("This is a game of rock paper scissors! It is a best of 5 against a computer!")
+    gameplay.innerText = "This is a game of rock paper scissors! It is a best of 5 against a computer!";
     computerScore = 0;
     playerScore = 0;
-    while( computerScore < 3 & playerScore < 3) {
-        playerSelection = prompt("What is your selection?")
-        result = playRound(playerSelection)
-        console.log(result);
-        console.log(`The score is Player: ${playerScore} Computer: ${computerScore}`);
-    }
+    results.innerText = `The score is Player: ${playerScore} Computer: ${computerScore}`;
 
-    console.log(computerScore == 3 ? "The computer wins!": "You win!");
+
+    //while( computerScore < 3 & playerScore < 3) {
+        // playerSelection = prompt("What is your selection?")
+        // result = playRound(playerSelection)
+        // console.log(result);
+    //}
+
+    //console.log(computerScore == 3 ? "The computer wins!": "You win!");
 }
